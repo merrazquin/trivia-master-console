63ab35d903ab4c91abe52ed41aa22d0ed6303e0c
Marcela Errazquin <merrazquin@gmail.com>
2018-03-28 22:30:42 -0400

    Created dashboard js, including sign out method

---
+++ scripts/dashboard.js
@@ -0,0 +1,8 @@
+$("#logout").click(() => {
+    firebase.auth().signOut().then(function () {
+        console.log('Signed Out');
+        window.location.replace("index.html");
+    }, function (error) {
+        console.error('Sign Out Error', error);
+    });
+});
